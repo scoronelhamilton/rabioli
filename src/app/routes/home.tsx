9b6b007337ef31084900f80724b2a3feaf3f4580
Sebastian Coronel <scoronelhamilton@gmail.com>
2025-07-03 14:51:32 -0500

    cleanup

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,13 +1,9 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: "Rabioli" }]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Hello World!</div>
 }
